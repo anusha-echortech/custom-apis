af0ed55628acc7bf26d9b7d3a0202d6874985c6e
anusha <anusha@echortech.com>
2025-07-11 15:34:18 +0530

    generic api

--- server.js
+++ server.js
@@ -8,125 +8,85 @@ app.use(express.json());
 const dbFile = './db.json';
 
 function readDB() {
+  if (!fs.existsSync(dbFile)) {
+    fs.writeFileSync(dbFile, JSON.stringify({}, null, 2));
+  }
   const data = fs.readFileSync(dbFile);
   return JSON.parse(data);
 }
+
 
 function writeDB(data) {
   fs.writeFileSync(dbFile, JSON.stringify(data, null, 2));
 }
 
-// --- POSTS ---
 
-// GET all posts
-app.get('/posts', (req, res) => {
+
+app.get('/:resource', (req, res) => {
+  const { resource } = req.params;
   const db = readDB();
-  res.json(db.posts);
+  const data = db[resource];
+  if (!data) return res.status(404).json({ message: 'Resource not found' });
+  res.json(data);
 });
 
-// GET post by ID
-app.get('/posts/:id', (req, res) => {
+
+app.get('/:resource/:id', (req, res) => {
+  const { resource, id } = req.params;
   const db = readDB();
-  const post = db.posts.find(p => p.id === parseInt(req.params.id));
-  post ? res.json(post) : res.status(404).json({ message: 'Post not found' });
+  const data = db[resource];
+  if (!data) return res.status(404).json({ message: 'Resource not found' });
+  const item = data.find(i => i.id === parseInt(id));
+  if (!item) return res.status(404).json({ message: 'Item not found' });
+  res.json(item);
 });
 
-// CREATE new post
-app.post('/posts', (req, res) => {
+
+app.post('/:resource', (req, res) => {
+  const { resource } = req.params;
   const db = readDB();
-  const newPost = req.body;
-  newPost.id = db.posts.length ? db.posts[db.posts.length - 1].id + 1 : 1;
-  db.posts.push(newPost);
+  if (!db[resource]) db[resource] = [];
+  const newItem = req.body;
+  newItem.id = db[resource].length ? db[resource][db[resource].length - 1].id + 1 : 1;
+  db[resource].push(newItem);
   writeDB(db);
-  res.status(201).json(newPost);
+  res.status(201).json(newItem);
 });
 
-// UPDATE post
-app.put('/posts/:id', (req, res) => {
-  const db = readDB();
-  const index = db.posts.findIndex(p => p.id === parseInt(req.params.id));
-  if (index !== -1) {
-    db.posts[index] = { ...db.posts[index], ...req.body };
-    writeDB(db);
-    res.json(db.posts[index]);
-  } else {
-    res.status(404).json({ message: 'Post not found' });
-  }
-});
 
-// DELETE post
-app.delete('/posts/:id', (req, res) => {
+app.put('/:resource/:id', (req, res) => {
+  const { resource, id } = req.params;
   const db = readDB();
-  const index = db.posts.findIndex(p => p.id === parseInt(req.params.id));
-  if (index !== -1) {
-    const deleted = db.posts.splice(index, 1);
-    writeDB(db);
-    res.json(deleted[0]);
-  } else {
-    res.status(404).json({ message: 'Post not found' });
-  }
-});
-
-// --- COMMENTS ---
-
-// GET all comments
-app.get('/comments', (req, res) => {
-  const db = readDB();
-  res.json(db.comments);
-});
-
-// GET comment by ID
-app.get('/comments/:id', (req, res) => {
-  const db = readDB();
-  const comment = db.comments.find(c => c.id === parseInt(req.params.id));
-  comment ? res.json(comment) : res.status(404).json({ message: 'Comment not found' });
-});
-
-// CREATE new comment
-app.post('/comments', (req, res) => {
-  const db = readDB();
-  const newComment = req.body;
-  newComment.id = db.comments.length ? db.comments[db.comments.length - 1].id + 1 : 1;
-  db.comments.push(newComment);
+  const data = db[resource];
+  if (!data) return res.status(404).json({ message: 'Resource not found' });
+  const index = data.findIndex(i => i.id === parseInt(id));
+  if (index === -1) return res.status(404).json({ message: 'Item not found' });
+  data[index] = { ...data[index], ...req.body };
   writeDB(db);
-  res.status(201).json(newComment);
+  res.json(data[index]);
 });
 
-// UPDATE comment
-app.put('/comments/:id', (req, res) => {
+
+app.delete('/:resource/:id', (req, res) => {
+  const { resource, id } = req.params;
   const db = readDB();
-  const index = db.comments.findIndex(c => c.id === parseInt(req.params.id));
-  if (index !== -1) {
-    db.comments[index] = { ...db.comments[index], ...req.body };
-    writeDB(db);
-    res.json(db.comments[index]);
-  } else {
-    res.status(404).json({ message: 'Comment not found' });
-  }
+  const data = db[resource];
+  if (!data) return res.status(404).json({ message: 'Resource not found' });
+  const index = data.findIndex(i => i.id === parseInt(id));
+  if (index === -1) return res.status(404).json({ message: 'Item not found' });
+  const deleted = data.splice(index, 1);
+  writeDB(db);
+  res.json(deleted[0]);
 });
 
-// DELETE comment
-app.delete('/comments/:id', (req, res) => {
-  const db = readDB();
-  const index = db.comments.findIndex(c => c.id === parseInt(req.params.id));
-  if (index !== -1) {
-    const deleted = db.comments.splice(index, 1);
-    writeDB(db);
-    res.json(deleted[0]);
-  } else {
-    res.status(404).json({ message: 'Comment not found' });
-  }
-});
 
-// --- PROFILE ---
 
-// GET profile
 app.get('/profile', (req, res) => {
   const db = readDB();
+  if (!db.profile) db.profile = {};
   res.json(db.profile);
 });
 
-// UPDATE profile (PUT)
 app.put('/profile', (req, res) => {
   const db = readDB();
   db.profile = { ...db.profile, ...req.body };
@@ -136,5 +96,5 @@ app.put('/profile', (req, res) => {
 
 // --- Start server ---
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`✅ Server running at http://localhost:${port}`);
 });
